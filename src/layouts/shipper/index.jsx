62ed9e85c8ae35464368616d4a45914d76c1c1fd
hoang1101 <lhhoang1101@gmail.com>
2022-12-20 20:19:16 +0700

    shipper

--- src/layouts/shipper/index.jsx
+++ src/layouts/shipper/index.jsx
@@ -1,51 +1,67 @@
-import { Layout, Menu } from 'antd';
-import React, { useState } from 'react';
-import { routerLinks } from '@/utils';
-import { useNavigate } from 'react-router';
+import { Card, Layout, Menu } from "antd";
+import React, { useState } from "react";
+import { routerLinks } from "@/utils";
+import { useNavigate } from "react-router";
 
-import listMenu from './menus';
+import listMenu from "./menus";
 
-import { keyToken } from '@/variable';
+import { keyToken } from "@/variable";
+import { useAuth } from "@/global";
+import "./index.less";
 const { Content, Footer, Sider } = Layout;
 
 const App = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
+  const auth = useAuth();
   return (
     <Layout
       style={{
-        minHeight: '100vh',
+        minHeight: "100vh",
+        overflow: "hidden",
       }}
     >
-      <Sider
-        style={{
-          background: '#ECECEC',
-        }}
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
-      >
-        <div>dsadsadasd</div>
+      <Sider className="sider">
+        <div className="cardName">
+          <h1>GHN</h1>
+          <div className="abc">
+            <img
+              style={{ width: 35, height: 35, borderRadius: 50, marginTop: 5 }}
+              src="https://scr.vn/wp-content/uploads/2020/07/Avatar-Facebook-tr%E1%BA%AFng.jpg"
+              alt="logoMale"
+            />
+            <div style={{ paddingLeft: 5 }}>
+              <div className="nameUser">Huan</div>
+              <div>huan@gmail.com</div>
+            </div>
+          </div>
+        </div>
         <Menu
-          style={{
-            background: '#ECECEC',
-          }}
-          defaultSelectedKeys={['1']}
+          className="menu"
+          defaultSelectedKeys={["1"]}
           onClick={(info) => {
             !localStorage.getItem(keyToken)
-              ? navigate(routerLinks('Login'), { replace: true })
+              ? navigate(routerLinks("Login"), { replace: true })
               : navigate(routerLinks(info.key));
           }}
           mode="inline"
           items={listMenu}
         />
-        <div onClick={() => navigate(routerLinks('Login'))}>logout</div>
+        <div
+          className="logout"
+          onClick={() => {
+            auth.logout();
+            navigate(routerLinks("Login"));
+          }}
+        >
+          logout
+        </div>
       </Sider>
       <Layout className="site-layout">
         <Content>{children}</Content>
         <Footer
           style={{
-            textAlign: 'center',
+            textAlign: "center",
           }}
         >
           Ant Design ©2018 Created by Ant UED
